e31f11811cb1ce17356f7939321959bd9ec3697a
William Lima <williamlims@hotmail.com>
2023-04-04 19:09:01 -0300

    Adding react-dotenv for environment variables

---
+++ src/pages/login/Login.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import Container from 'react-bootstrap/Container';
+//import Row from 'react-bootstrap/Row';
+//import Col from 'react-bootstrap/Col';
+//import Button from 'react-bootstrap/Button';
+
+function Login() {
+    const style = {
+        position: "center",
+        align: "center",
+        width: "250px",
+        backgroundColor: "black"
+    };
+    return (
+        <Container >
+            olá
+        </Container>
+    );
+}
+
+export default Login;
