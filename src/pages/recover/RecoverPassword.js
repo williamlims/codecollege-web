922c4c0502b6d457323fc7c59f4e56245a746e28
William Lima <williamlims@hotmail.com>
2023-04-12 22:15:06 -0300

    Styling register, recover e login pages

--- src/pages/recover/RecoverPassword.js
+++ src/pages/recover/RecoverPassword.js
@@ -1,13 +1,39 @@
 import * as React from 'react';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import NavBarLight from '../../components/navbarlight/NavBarLight';
+import Image from 'react-bootstrap/Image';
+import img from '../../assets/logoLogin.png';
 
 function RecoverPassword() {
     return (
         <>
             <NavBarLight />
-            <Container >
-                RecoverPassword
+            <Container className="mt-5 shadow" style={{ maxWidth: '360px', borderRadius: 10}}>
+                <Form style={{marginLeft:15, marginRight: 15}}>
+                    <br />
+                    <Form.Group className="mb-3"  id="groupOptions">
+                        <Image style={{width:'65%'}} src={img}/>
+                    </Form.Group>
+                    <Row className="mt-3 mb-3">
+                        <Form.Group as={Col} controlId="emailRecover">
+                            <Form.Control type="email" placeholder="Insira seu e-mail" />
+                        </Form.Group>
+                    </Row>
+
+                    <Button className="w-100" variant="primary" type="submit">
+                        Enviar link
+                    </Button>
+
+                    <Form.Group className="mb-3"  id="groupOptions">
+                        <Form.Text style={{color:'green'}}>Link para recuperação de senha enviado!</Form.Text>
+                    </Form.Group>
+                    <br />
+                </Form>
+                
             </Container>
         </>
     );
--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -9,62 +9,63 @@ import Nav from 'react-bootstrap/Nav';
 import { NavLink } from 'react-router-dom';
 
 function Register() {
+    const styles = {
+        marginsFildes: {
+            marginLeft:15, 
+            marginRight:15
+        },
+        colorFieldData: {
+            color:'gray'
+        }
+    };
+
     return (
         <>
             <NavBarLight />
-            <Container className="mt-3">
+            <Container className="mt-4 shadow" style={{ borderRadius: 10}}>
                 <Form>
-                    <Row className="mb-3">
-                        <Form.Group as={Col} controlId="formGridEmail">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        </Form.Group>
+                    < br/>
+                    <Row className="mb-3" style={styles.marginsFildes}>
+                        <Form.Text>Novo cadastro</Form.Text>
+                    </Row>
 
-                        <Form.Group as={Col} controlId="formGridPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                    <Row className="mb-4" style={styles.marginsFildes}>
+                        <Form.Group as={Col} controlId="nameUser">
+                            <Form.Control type="text" placeholder="Insira seu nome"></Form.Control>
                         </Form.Group>
                     </Row>
 
-                    <Form.Group className="mb-3" controlId="formGridAddress1">
-                        <Form.Label>Address</Form.Label>
-                        <Form.Control placeholder="1234 Main St" />
-                    </Form.Group>
-
-                    <Form.Group className="mb-3" controlId="formGridAddress2">
-                        <Form.Label>Address 2</Form.Label>
-                        <Form.Control placeholder="Apartment, studio, or floor" />
-                    </Form.Group>
-
-                    <Row className="mb-3">
-                        <Form.Group as={Col} controlId="formGridCity">
-                        <Form.Label>City</Form.Label>
-                        <Form.Control />
-                        </Form.Group>
-
-                        <Form.Group as={Col} controlId="formGridState">
-                        <Form.Label>State</Form.Label>
-                        <Form.Select defaultValue="Choose...">
-                            <option>Choose...</option>
-                            <option>...</option>
-                        </Form.Select>
-                        </Form.Group>
-
-                        <Form.Group as={Col} controlId="formGridZip">
-                        <Form.Label>Zip</Form.Label>
-                        <Form.Control />
+                    <Row className="mb-4" style={styles.marginsFildes}>
+                        <Form.Group as={Col} controlId="surnameUser">
+                            <Form.Control type="text" placeholder="Insira seu sobrenome" />
                         </Form.Group>
                     </Row>
 
-                    <Form.Group className="mb-3" id="formGridCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
+                    <Row className="mb-4" style={styles.marginsFildes}>
+                        <Form.Group as={Col} controlId="birthdayUser">
+                            <Form.Control type="date" style={styles.colorFieldData} />
+                        </Form.Group>
+                    </Row>
 
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
+                    <Row className="mb-4" style={styles.marginsFildes}>
+                        <Form.Group as={Col} controlId="surnameUser">
+                            <Form.Control type="password" placeholder="Senha" />
+                        </Form.Group>
+                        <Form.Group as={Col} controlId="surnameUser">
+                            <Form.Control type="password" placeholder="Repetir senha" />
+                        </Form.Group>
+                    </Row>
+
+                    <Row className="mb-4" style={styles.marginsFildes}>
+                        <Form.Group as={Col} controlId="btnSave">
+                            <Button variant="primary" type="submit">
+                                Cadastrar
+                            </Button>
+                        </Form.Group>
+                    </Row>
+                    < br/>
+                    < br/>
                 </Form>
-                <Nav.Link to="/home" key="home" as={NavLink}>Home</Nav.Link>
             </Container>
         </>
     );
