cb180bfeaf6df971d6ca4f6986d273a5b97cd76b
William Lima <williamlims@hotmail.com>
2023-04-27 22:22:04 -0300

    update page courses

--- src/pages/home/courses/Courses.js
+++ src/pages/home/courses/Courses.js
@@ -5,8 +5,12 @@ import NavMain from '../../../components/navMain/NavMain';
 import Footer from '../../../components/footer/Footer';
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
 import Nav from 'react-bootstrap/Nav';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { NavLink } from 'react-router-dom';
 import TabInfo from '../../../components/tab/TabInfo';
+import user from '../../../assets/userMain.png';
+import Button from 'react-bootstrap/Button';
 
 function Courses() {
     const path = { 
@@ -22,8 +26,22 @@ function Courses() {
             <NavMain nameUser="Marcos Luiz" messageUser="Cursos disponíveis na plataforma."/>
             <BreadcrumbPath path={path.element}/>
             <TabInfo info="Cursos"/>
-            <Container style={{height:800}}>
-                Cursos
+            <Container style={{height:600}} className='overflow-auto'>
+                <Row className='shadow mt-2 p-2'>
+                    <Col sm={3} xs={2} md={2} lg={2} xl={1} className='d-flex align-items-center'>
+                        <img src={user} width="80" height="80" 
+                                style={{borderBlockColor:'black'}} className=" shadow d-inline-block align-top" alt="pho" />
+                    </Col>
+                    <Col sm={5} xs={12} md={6} lg={6} xl={6} className='text-break m-2 p-1'>
+                        <span style={{fontFamily:'Arial Black', fontSize: 16}}>Nome Curso</span> < br/> 
+                        <span style={{fontFamily:'Arial', fontSize: 14}}>fdadfb dbdbdhrehe hrheherhe rhreherhrehe herheherher hehr eherhr eherh shdf hdddgf gsdgf dsgfsdgd sfgsd gfdgd fsg khj jh gjhf gdf gd dfg dfdfd fdfdf dfdfd </span>
+                    </Col>
+                    <Col sm={3} xs={4} md={4} lg={3} xl={3} className='m-2 p-1 d-flex align-items-center'>
+                        <Button as={NavLink} to="/home/courses/5" variant="primary">
+                            Entrar no curso
+                        </Button>
+                    </Col>
+                </Row>
             </Container>
             <Footer />
             
