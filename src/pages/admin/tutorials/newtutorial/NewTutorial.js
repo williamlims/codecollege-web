1b42a085db80acaf35e13cf803dd2ee3c771dd38
William Lima <williamlims@hotmail.com>
2023-05-16 22:47:04 -0300

    finishing upload file in tutorials

--- src/pages/admin/tutorials/newtutorial/NewTutorial.js
+++ src/pages/admin/tutorials/newtutorial/NewTutorial.js
@@ -5,23 +5,25 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import InputGroup from 'react-bootstrap/InputGroup';
-
+import api from "../../../../services/api";
 
 function NewTutorial() {
-
+    const [file, setFile] = useState();
     const [cmShow, setCmShow] = useState(false);
-    const [infonameCourse, setInfonameCourse] = useState("Por favor, insira o nome do curso!");
-    const [infonameCourseColor, setInfonameCourseColor] = useState("gray");
-
-    const [infodescriptionCourse, setInfodescriptionCourse] = useState("Por favor, insira uma descrição para o curso!");
-    const [infodescriptionCourseColor, setInfodescriptionCourseColor] = useState("gray");
-
+    const [infotutorialName, setInfotutorialName] = useState("Por favor, insira o nome do tutorial!");
+    const [infotutorialNameColor, setInfotutorialNameColor] = useState("gray");
+    const [infofileDocument, setInfofileDocument] = useState("Envie o documento referente ao tutorial (ex: tutorial.pdf)!");
+    const [infofileDocumentColor, setInfofileDocumentColor] = useState("gray");
+    const [infolevelTutorial, setInfolevelTutorial] = useState("Escolha o nível do tutorial!");
+    const [infolevelTutorialColor, setInfolevelTutorialColor] = useState("gray");
+    const [infotutotialSubject, setInfotutotialSubject] = useState("Escolha o assunto do tutorial!");
+    const [infotutotialSubjectColor, setInfotutotialSubjectColor] = useState("gray");
 
     const style = {
-        nameCourseColor:{color:infonameCourseColor, borderColor:infonameCourseColor},
-        descriptionCourseColor:{color:infodescriptionCourseColor, borderColor:infodescriptionCourseColor},
-        
+        tutorial:{color:infotutorialNameColor, borderColor:infotutorialNameColor},
+        file:{color:infofileDocumentColor, borderColor:infofileDocumentColor},
+        level:{color:infolevelTutorialColor, borderColor:infolevelTutorialColor},
+        subject:{color:infotutotialSubjectColor, borderColor:infotutotialSubjectColor}, 
     };
 
     const returnID = () => {
@@ -31,41 +33,104 @@ function NewTutorial() {
                    date.getMilliseconds());
     };
 
-    const backToStartNameCourse = () => {
-        setInfonameCourseColor("gray");
-        setInfonameCourse("Por favor, insira o nome do curso!");
+    const saveFile = () => {
+        let formData = new FormData();
+        let doc = document.getElementById("fileDocument");
+        formData.append("fileDocument", doc.files[0]);
+        api.post('/v1/tutorials/file', formData, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+        }).then((response) => {
+            if(response.status === 200){
+                setFile(response.data.message);
+            }
+        }).catch((error) => {
+            return error;
+        });
     };
 
-    const backToStartSurname = () => {
-        infodescriptionCourseColor("gray");
-        infodescriptionCourse("Por favor, insira o sobrenome de usuário!");
+    const saveTutorial = () => {
+        let controlIdTutorial = document.getElementById("controlIdTutorial").value;
+        let tutorialName = document.getElementById("tutorialName").value;
+        let levelTutorial = document.getElementById("levelTutorial").value;
+        let tutotialSubject = document.getElementById("tutotialSubject").value;
+        let fnPath = document.getElementById("finalPath").value;
+
+        saveFile();
+
+        alert(fnPath);
+
+        api.post('/v1/tutorials/', {
+            idControl: controlIdTutorial,
+            nameTutorial: tutorialName,
+            level: levelTutorial,
+            subject: tutotialSubject,
+            filePath: fnPath
+        }).then(res => {
+            return res.status;
+        }).catch(error => {
+            return error;
+        });
+
+    };
+
+    const backToStartTutorial = () => {
+        setInfotutorialNameColor("gray");
+        setInfotutorialName("Por favor, insira o nome do tutorial!");
+    };
+
+    const backToStartFile = () => {
+        setInfofileDocumentColor("gray");
+        setInfofileDocument("Envie o documento referente ao tutorial (ex: tutorial.pdf)!");
+    };
+
+    const backToStartLevel= () => {
+        setInfolevelTutorialColor("gray");
+        setInfolevelTutorial("Escolha o nível do tutorial!");
+    };
+
+    const backToStartSubject = () => {
+        setInfotutotialSubjectColor("gray");
+        setInfotutotialSubject("Escolha o assunto do tutorial!");
     };
 
     const clearForm = () => {
-        backToStartNameCourse();
-        backToStartSurname();
+        backToStartTutorial();
+        backToStartFile();
+        backToStartLevel();
+        backToStartSubject();
     }
 
     const submitValidate = () => {
-        let nameCourse = document.getElementById("nameCourse").value;
-        let descriptionCourse = document.getElementById("descriptionCourse").value;
-        let levelCourse = document.getElementById("levelCourse").value;
-        let areaCourse = document.getElementById("areaCourse").value;
+        let controlIdTutorial = document.getElementById("controlIdTutorial");
+        let tutorialName = document.getElementById("tutorialName");
+        let fileDocument = document.getElementById("fileDocument");
+        let levelTutorial = document.getElementById("levelTutorial");
+        let tutotialSubject = document.getElementById("tutotialSubject");
         
-        if (nameCourse === ""){
-            setInfonameCourse("Você esqueceu de inserir o nome do curso!");
-            setInfonameCourseColor("red");
-        } else if (descriptionCourse === ""){
-            setInfodescriptionCourse("Você esqueceu de inserir uma descrição para o curso!");
-            infodescriptionCourseColor("red");
-        } else if (levelCourse === "----------"){
-
-        } else if (areaCourse === "----------"){
-
+        if (tutorialName.value === ""){
+            setInfotutorialName("Você esqueceu de inserir o nome do tutorial!");
+            setInfotutorialNameColor("red");
+        } else if (fileDocument.value === ""){
+            setInfofileDocument("Você esqueceu de inserir o arquivo!");
+            setInfofileDocumentColor("red");
+        } else if (levelTutorial.value === "----------"){
+            setInfolevelTutorial("Você esqueceu de escolher o nível!");
+            setInfolevelTutorialColor("red");
+        } else if (tutotialSubject.value === "----------"){
+            setInfotutotialSubject("Você esqueceu de escolher o assunto!");
+            setInfotutotialSubjectColor("red");
         } else {
-            // set the database tasks here
+            saveTutorial();
             clearForm();
             setCmShow(true);
+            setFile('');
+            controlIdTutorial.value = returnID();
+            tutorialName.value = "";
+            fileDocument.value = "";
+            levelTutorial.value = "----------";
+            tutotialSubject.value = "----------";
         }
         
     };
@@ -73,27 +138,44 @@ function NewTutorial() {
     return (
         <>
             <Container fluid className='p-2 shadow-sm'>
-                <Form as={Col} xl={8} lg={12} md={12} sm={12} es={12}>
+                <Modal
+                    size="sm"
+                    show={cmShow}
+                    onHide={() => setCmShow(false)}
+                    aria-labelledby="confirmation"
+                >
+                    <Modal.Header closeButton>
+                    <Modal.Title id="confirmation">
+                        Confirmação
+                    </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body className='d-flex flex-column justify-content-center'>
+                        <p>Tutorial criado com sucesso!</p>
+                        <Button variant="dark" onClick={() => {setCmShow(false)}} className='mt-2 m-3'>OK</Button>
+                    </Modal.Body>
+                </Modal>
+                <Form action='' as={Col} xl={8} lg={12} md={12} sm={12} es={12} encType="multipart/form-data">
                     <Form.Group className="mb-3 mt-2" controlId="controlIdTutorial">
                         <Form.Control type="text" disabled value={returnID()}/>
-                        <Form.Text style={style.conrtolIDColor}>
+                        <Form.Text>
                             {`Este é o ID de controle deste tutorial!`}
                         </Form.Text>
                     </Form.Group>
                     <Form.Group className="mb-3 mt-2" controlId="tutorialName">
-                        <Form.Control type="text" placeholder="Nome do tutorial"/>
-                        <Form.Text style={style.conrtolIDColor}>
-                            {`Por favor, insira o nome do tutorial!`}
+                        <Form.Control type="text" style={style.tutorial} placeholder="Nome do tutorial"/>
+                        <Form.Text style={style.tutorial}>
+                            {infotutorialName}
                         </Form.Text>
                     </Form.Group>
-                    <Form.Group className="mb-3 mt-2" controlId="fileDocument">
-                        <Form.Control type="file" placeholder="Envie o arquivo"/>
-                        <Form.Text style={style.conrtolIDColor}>
-                            {`Envie o documento referente ao tutorial (ex: tutorial.pdf)!`}
+                    <Form.Group className="mb-3 mt-2" name="fileDocument" controlId="fileDocument">
+                        <input type="hidden" id="finalPath" name="finalPath" value={file} />
+                        <Form.Control type="file" name="fileDocument" placeholder="Envie o arquivo"/>
+                        <Form.Text style={style.level}>
+                            {infofileDocument}
                         </Form.Text>
                     </Form.Group>
                     <Form.Group className="mb-3">
-                        <Form.Select id="levelTutorial" style={style.levelCourseColor}>
+                        <Form.Select id="levelTutorial" style={style.level}>
                             <option>----------</option>
                             <option value={1}>Iniciante</option>
                             <option value={2}>Intermediário</option>
@@ -101,11 +183,11 @@ function NewTutorial() {
                             <option value={4}>Especialista</option>
                         </Form.Select>
                         <Form.Text style={style.levelCourseColor}>
-                            {`Por favor, escolha o módulo que está aula fará parte!`}
+                            {infolevelTutorial}
                         </Form.Text>
                     </Form.Group>
                     <Form.Group className="mb-4">
-                        <Form.Select id="tutotialSubject" style={style.areaCourseColor}>
+                        <Form.Select id="tutotialSubject" style={style.subject}>
                             <option>----------</option>
                             <option value={1}>Tecnologia</option>
                             <option value={2}>Administração</option>
@@ -116,12 +198,12 @@ function NewTutorial() {
                             <option value={7}>Comunicação</option>
                             <option value={8}>Outro</option>
                         </Form.Select>
-                        <Form.Text style={style.areaCourseColor}>
-                            {`Por favor, escolha o assunto deste tutorial!`}
+                        <Form.Text style={style.subject}>
+                            {infotutotialSubject}
                         </Form.Text>
                     </Form.Group>
                     <Row className="mb-4">
-                        <Form.Group as={Col} controlId="btnSaveCourse">
+                        <Form.Group as={Col} controlId="btnSaveTutorial">
                             <Button variant="dark" as={Col} md={2} lg={2} sm={2} onClick={() => submitValidate()} type="submit" >
                                 Salvar
                             </Button>
